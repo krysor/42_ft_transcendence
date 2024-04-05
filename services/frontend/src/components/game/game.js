da9f7610d441d846f4cd14688eccaf39626753ff
krysor <krysor901@gmail.com>
2024-04-05 18:04:25 +0200

    created basic container for the game

--- services/frontend/src/components/game/game.js
+++ services/frontend/src/components/game/game.js
@@ -1,9 +1,17 @@
 import React from "react";
+import './game.css'
 
+import circle_icon from '../assets/ball.png'
 
 function Game() {
 	return (
-			<h1>Game</h1>
+		<div className='container'>
+			<h1>Ping pong <span>game</span></h1>
+			<div className='board'>
+				<p>test</p>
+			</div>
+			<button className="pause">Pause</button>
+		</div>
 	);
 }
 
