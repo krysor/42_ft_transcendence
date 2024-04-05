5880da3c999680f9e73525dae90c595d037ac8d7
Oran Dan <odan@shi-r3-p5.s19.be>
2024-04-05 17:05:11 +0200

    feat: added healthchecks

--- services/backend/wait.sh
+++ services/backend/wait.sh
@@ -5,6 +5,16 @@ while ! PGPASSWORD=postgres psql -h db -p 5432 -U root -d postgres; do
 	sleep 5
 done
 
+# Check if superuser exists, if not, create it
+if ! python -c "from django.contrib.auth.models import User; \
+                User.objects.filter(username='admin2').exists()"; then
+    python manage.py createsuperuser --username=admin2 --email "" --noinput
+fi
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
+
 echo "postgreSQL connection succeed !"
 
 exec "$@"
