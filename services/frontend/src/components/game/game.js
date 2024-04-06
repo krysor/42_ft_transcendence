dea63cf4fb11572eccbb6a64b1b57cdb8e108c25
krysor <krysor901@gmail.com>
2024-04-06 22:14:15 +0200

    created the basic visual structures necessary to implement the game

--- services/frontend/src/components/game/game.js
+++ services/frontend/src/components/game/game.js
@@ -1,14 +1,16 @@
 import React from "react";
 import './game.css'
 
-import circle_icon from '../assets/ball.png'
-
 function Game() {
 	return (
 		<div className='container'>
-			<h1>Ping pong <span>game</span></h1>
+			<h1>Ping pong game</h1>
 			<div className='board'>
-				<p>test</p>
+				<div id="upper" className="border"></div>
+				<div id="lower" className="border"></div>
+				<div id="left" className="pad"></div>
+				<div id="right" className="pad"></div>
+				<div className="ball"></div>
 			</div>
 			<button className="pause">Pause</button>
 		</div>
