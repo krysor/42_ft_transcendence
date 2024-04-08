b8b4019b019392d833d09804530c2cd3efbf6c3b
PearceKof <benjaminlaurentpro00@gmail.com>
2024-04-08 12:59:58 +0200

    copy package.json in the dockerfile

--- services/backend/project/pong/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import main_js
-
-urlpatterns = [
-    path('', main_js),
-]
--- services/backend/project/project/settings.py
+++ services/backend/project/project/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-from api.authentification import FortyTwoAuthentication
+# from api.authentification import FortyTwoAuthentication
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,8 +51,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'api',
-    'pong',
     'authentication',
     'corsheaders',
 ]
