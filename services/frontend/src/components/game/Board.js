ca3c2a7bbaa6434a7a57fd292162df71c3208460
krysor <krysor901@gmail.com>
2024-04-15 16:51:19 +0200

    vertical bounce working and animating properly, start/pause button added and working

--- services/frontend/src/components/game/Board.js
+++ services/frontend/src/components/game/Board.js
@@ -1,69 +1,89 @@
 import React from 'react';
 
-const 	widthBoard		= 800;
-const 	heightBoard		= 400;
-// 50 before
-const	heightPad		= 100;
-const	widthPad		= 10;
-const	heightBorder	= 10;
-const 	radiusBall		= 30;
+const 	boardWidth		= 800;
+const 	boardHeight		= 400;
+const	borderHeight	= 10;
+const	padHeight		= 100;
+const	padWidth		= 10;
+const 	ballDiameter	= 30;
 
 const Border = (posY) => {
 	return (<div className="border"
-				 style={{'width' 	: widthBoard,
-						 'height'	: heightBorder,
+				 style={{'width' 	: boardWidth,
+						 'height'	: borderHeight,
 						 'top'	 	: posY}}></div>)
 }
 
 const Pad = (posX, posY) => {
 	return (<div className="pad"
-				 style={{'width'	: widthPad,
-						 'height'	: heightPad, 
+				 style={{'width'	: padWidth,
+						 'height'	: padHeight, 
 				 		 'left'	  	: posX,		 
 				 		 'top'	  	: posY}}></div>)
 }
 
 const Ball = (posX, posY) => {
 	return (<div id='ball'
-				 style={{'width'	: radiusBall,
-				 		 'height'	: radiusBall,
+				 style={{'width'	: ballDiameter,
+				 		 'height'	: ballDiameter,
 						 'left'		: Math.round(posX),
 						 'top'		: Math.round(posY)}}></div>)
 }
 
-const Board = (paused) => {
-	const padY = (heightBoard - heightPad)/2;
-	const [posX, setPosX] = React.useState((widthBoard - radiusBall)/2);
-	const [posY, setPosY] = React.useState((heightBoard - radiusBall)/2);
-	const vXBall = React.useRef(5);
-	const vYBall = React.useRef(5);
-	const frameId = React.useRef(0);
+const BallHitHorizontalBorder = (ballPosY) => {
+	const borderUpper = borderHeight;
+	const borderLower = boardHeight - borderHeight - ballDiameter;
 	
-	const animate = time => {
-		if (posY <= heightBorder || posY >= heightBoard - heightBorder - radiusBall)
-			vYBall.current = vYBall.current * -1;
-		setPosX(posX + vXBall.current);
-		setPosY(posY + vYBall.current);
+	if (ballPosY <= borderUpper
+		|| ballPosY >= borderLower)
+		return (true);
+	return (false);
+}
+
+const BallHitPad = (ballPosX, ballPosY, padLeftPosY, padRightPosY) => {
+	
+	if ((ballPosX <= padWidth && ballPosY + ballDiameter >= padLeftPosY && ballPosY <= padLeftPosY + padHeight)
+		|| (ballPosX >= boardWidth - padWidth - ballDiameter && ballPosY + ballDiameter >= padRightPosY && ballPosY <= padRightPosY + padHeight))
+		return (true);
+	return (false);
+}
+
+const Board = (paused) => {
+	const [ballPosX, setBallPosX] = React.useState((boardWidth - ballDiameter)/2);
+	const [ballPosY, setBallPosY] = React.useState((boardHeight - ballDiameter)/2);
+	const ballSpeedX = React.useRef(-5);
+	const ballSpeedY = React.useRef(5);
+	const frameId 	 = React.useRef(0);
+
+	const padLeftPosY = (boardHeight - padHeight)/2;
+	const padRightPosY = (boardHeight - padHeight)/2;
+
+	const animate = () => {
+		if (BallHitHorizontalBorder(ballPosY))
+			ballSpeedY.current = ballSpeedY.current * -1;
+		if (BallHitPad(ballPosX, ballPosY, padLeftPosY, padRightPosY))
+			ballSpeedX.current = ballSpeedX.current * -1;		
+		setBallPosX(ballPosX + ballSpeedX.current);
+		setBallPosY(ballPosY + ballSpeedY.current);
+
 		frameId.current = requestAnimationFrame(animate);
 	}
-
-
 	React.useEffect(() => {
 		if (!paused)
 			frameId.current = requestAnimationFrame(animate);
 		else
 			cancelAnimationFrame(frameId.current);
 		return () => cancelAnimationFrame(frameId);
-	  }, [paused, posX, posY]);
+	  }, [paused, ballPosX, ballPosY]);
 
 
-	return (<div className='board' style={{'width':widthBoard,
-										   'height':heightBoard}}>
+	return (<div className='board' style={{'width':boardWidth,
+										   'height':boardHeight}}>
 				{Border(0)}
-				{Border(heightBoard - heightBorder)}
-				{Ball(posX, posY)}
-				{Pad(0, padY)}
-				{Pad(widthBoard - widthPad, padY)}
+				{Border(boardHeight - borderHeight)}
+				{Ball(ballPosX, ballPosY)}
+				{Pad(0, padLeftPosY)}
+				{Pad(boardWidth - padWidth, padRightPosY)}
 			</div>)
 }
 
