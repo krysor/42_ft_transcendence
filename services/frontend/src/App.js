408f7b748047698d4691cfeec934a1c3262f14ec
krysor <krysor901@gmail.com>
2024-04-24 14:36:31 +0200

    check if links in the navbar displaying correctly in this commit

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -20,7 +20,7 @@ import EditProfile from "./components/user/edit_profile"
 import NotFound from "./components/notfound";
 
 import "./App.css";
-import { Navbar, Container, Nav } from "react-bootstrap";
+// import { Navbar, Container, Nav } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProfilePic from "./components/user/getProfilePic";
 import UserPage from "./components/user/userPage";
@@ -40,23 +40,7 @@ function App () {
                     <span class="text-body-secondary">Toggleable via the navbar brand.</span>
                 </div>
             </div>
-              <Navbar bg="dark" variant="dark">
-                  <Container>
-                      <Nav className="me-auto">
-                      <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
-                        <Nav.Link as={Link} to="/Game">Game</Nav.Link>
-                        <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        {isLoggedIn &&<Nav.Link as={Link} to="/community">Community</Nav.Link>}
-                        {!isLoggedIn && <Nav.Link as={Link} to="/signup">Signup</Nav.Link>}
-                        {!isLoggedIn && <Nav.Link as={Link} to="/login">Login</Nav.Link>}
-                        {isLoggedIn && <Nav.Link as={Link} to="/logout">Logout</Nav.Link>}
-                      </Nav>
-                        {isLoggedIn &&
-                        <Nav.Link as={Link} to="/profile">
-                            {user && user.profile_pic && <ProfilePic filename={user.profile_pic} />}
-                        </Nav.Link>}
-                  </Container>
-              </Navbar>
+			<NavBar isLoggedIn={isLoggedIn} user={user}/>
               <Routes>
                   <Route path="/" element={<Home />} />
                   <Route path="/Game" element={<Game />} />
@@ -73,5 +57,52 @@ function App () {
           </Router>
       );
 }
+
+// function App () {
+// 	const token = sessionStorage.getItem('authtoken')
+// 	const user = JSON.parse(sessionStorage.getItem('user'))
+// 	const isLoggedIn = !!token;
+
+//   return (
+// 	  <Router>
+// 		<div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
+// 			<div class="bg-dark p-4">
+// 				<h5 class="text-body-emphasis h4">Collapsed content</h5>
+// 				<span class="text-body-secondary">Toggleable via the navbar brand.</span>
+// 			</div>
+// 		</div>
+// 		  <Navbar bg="dark" variant="dark">
+// 			  <Container>
+// 				  <Nav className="me-auto">
+// 				  <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+// 					<Nav.Link as={Link} to="/Game">Game</Nav.Link>
+// 					<Nav.Link as={Link} to="/about">About</Nav.Link>
+// 					{isLoggedIn &&<Nav.Link as={Link} to="/community">Community</Nav.Link>}
+// 					{!isLoggedIn && <Nav.Link as={Link} to="/signup">Signup</Nav.Link>}
+// 					{!isLoggedIn && <Nav.Link as={Link} to="/login">Login</Nav.Link>}
+// 					{isLoggedIn && <Nav.Link as={Link} to="/logout">Logout</Nav.Link>}
+// 				  </Nav>
+// 					{isLoggedIn &&
+// 					<Nav.Link as={Link} to="/profile">
+// 						{user && user.profile_pic && <ProfilePic filename={user.profile_pic} />}
+// 					</Nav.Link>}
+// 			  </Container>
+// 		  </Navbar>
+// 		  <Routes>
+// 			  <Route path="/" element={<Home />} />
+// 			  <Route path="/Game" element={<Game />} />
+// 			  <Route path="/about" element={<About />} />
+// 			  <Route path="/signup" element={<Signup />} />
+// 			  <Route path="/login" element={<Login />} />
+// 			  <Route path="/logout" element={<Logout />} />
+// 			  <Route path="/profile" element={<Profile />} />
+// 			  <Route path="/user_profile/:user_id" element={<UserPage />} />
+// 			  <Route path="/community" element={<Community />} />
+// 			  <Route path="/edit_profile" element={<EditProfile />} />
+// 			  <Route path="/*" element={<NotFound />} />
+// 		  </Routes>
+// 	  </Router>
+//   );
+// }
 
 export default App;
--- services/frontend/src/components/navbar.js
+++ services/frontend/src/components/navbar.js
@@ -1,27 +1,56 @@
-import { Link } from "react-router-dom";
-import { Nav } from "react-bootstrap";
+// import { Link } from "react-router-dom";
+// import { Nav } from "react-bootstrap";
 
-function NavBar() {
+import { Navbar, Container, Nav } from "react-bootstrap";
+import {
+    Link
+} from "react-router-dom";
+import ProfilePic from "./user/getProfilePic";
+
+
+function NavBar(props) {
 	return (
-		<nav id="navbar" class="navbar navbar-default">
-			<div class="container-fluid">
-				<div class="navbar-header">
-					<Nav.Link as={Link} to="/">
-						<a class="navbar-brand">
-							<img alt="" src="favicon.ico"></img>
-							ft_transcendence
-						</a>	
-					</Nav.Link>
-				</div>
-				<ul id="navlist" class="nav navbar-nav navbar-right">
-					<li><Nav.Link as={Link} to="/Home">Home</Nav.Link></li>
-					<li><Nav.Link as={Link} to="/Game">Game</Nav.Link></li>
-					<li><Nav.Link as={Link} to="/About">About</Nav.Link></li>
-					<li><Nav.Link as={Link} to="/Login">Login</Nav.Link></li>
-				</ul>
-			</div>
-		</nav>
+		<Navbar bg="dark" variant="dark">
+		<Container>
+			<Nav className="me-auto">
+			<Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+			<Nav.Link as={Link} to="/Game">Game</Nav.Link>
+			<Nav.Link as={Link} to="/about">About</Nav.Link>
+			{props.isLoggedIn &&<Nav.Link as={Link} to="/community">Community</Nav.Link>}
+			{!props.isLoggedIn && <Nav.Link as={Link} to="/signup">Signup</Nav.Link>}
+			{!props.isLoggedIn && <Nav.Link as={Link} to="/login">Login</Nav.Link>}
+			{props.isLoggedIn && <Nav.Link as={Link} to="/logout">Logout</Nav.Link>}
+			</Nav>
+			{props.isLoggedIn &&
+			<Nav.Link as={Link} to="/profile">
+				{props.user && props.user.profile_pic && <ProfilePic filename={props.user.profile_pic} />}
+			</Nav.Link>}
+		</Container>
+		</Navbar>
 	)
 }
+
+// function NavBar() {
+// 	return (
+// 		<nav id="navbar" class="navbar navbar-default">
+// 			<div class="container-fluid">
+// 				<div class="navbar-header">
+// 					<Nav.Link as={Link} to="/">
+// 						<a class="navbar-brand">
+// 							<img alt="" src="favicon.ico"></img>
+// 							ft_transcendence
+// 						</a>	
+// 					</Nav.Link>
+// 				</div>
+// 				<ul id="navlist" class="nav navbar-nav navbar-right">
+// 					<li><Nav.Link as={Link} to="/Home">Home</Nav.Link></li>
+// 					<li><Nav.Link as={Link} to="/Game">Game</Nav.Link></li>
+// 					<li><Nav.Link as={Link} to="/About">About</Nav.Link></li>
+// 					<li><Nav.Link as={Link} to="/Login">Login</Nav.Link></li>
+// 				</ul>
+// 			</div>
+// 		</nav>
+// 	)
+// }
 
 export default NavBar;
