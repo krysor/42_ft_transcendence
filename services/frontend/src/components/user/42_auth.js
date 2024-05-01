380fe8062c829402a21279bc3a3518c3324f1d91
Benjamin Laurent <blaurent@mi-r5-p1.s19.be>
2024-05-01 14:48:06 +0200

    error message when the 42 auth fail

--- services/frontend/src/components/user/42_auth.js
+++ services/frontend/src/components/user/42_auth.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Ft_auth = () => {
+    const [error, setError] = useState('');
     useEffect(() => {
         const urlParams = new URLSearchParams(window.location.search);
         const code = urlParams.get('code');
@@ -23,12 +24,26 @@ const Ft_auth = () => {
                     sessionStorage.setItem('user', JSON.stringify(data.user));
                     window.location.href = "/";
                 }
+                else if (data.error){
+					setError(data.error);
+				}
+                
 			})
             .catch(error => {
                 console.error('Error:', error);
             });
         }
     }, []);
+
+    if (error) {
+        return (
+            <div>
+                There was a problem while trying to authenticate.
+                <br />
+                {error && <div>{error}</div>}
+            </div>
+        );
+    }
 
     return (
         <div>
