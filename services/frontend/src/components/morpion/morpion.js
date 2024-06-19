54086933be8d0b131e721a0bf355c10ce627fbba
krysor <krysor@Laptop.local>
2024-06-19 13:43:46 +0200

    alternative solution to morpion squares remaining clickable while waiting for the bot to make a move

--- services/frontend/src/components/morpion/morpion.js
+++ services/frontend/src/components/morpion/morpion.js
@@ -346,7 +346,29 @@ function Board({ xIsNext, squares, onPlay }) {
 	if (!xIsNext && !winner && !draw) {
 		setTimeout(() => {
 			botPlayer();
-		}, 50);
+		}, 1000);
+
+		return (
+			<>
+				<div className="status">{status}</div>
+				<div className="board-row">
+					<Square value={squares[0]} />
+					<Square value={squares[1]} />
+					<Square value={squares[2]} />
+				</div>
+				<div className="board-row">
+					<Square value={squares[3]} />
+					<Square value={squares[4]} />
+					<Square value={squares[5]} />
+				</div>
+				<div className="board-row">
+					<Square value={squares[6]} />
+					<Square value={squares[7]} />
+					<Square value={squares[8]} />
+				</div>
+				<div className='restart'>{restartBtn}</div>
+			</>
+		);
 	}
 
 	return (
