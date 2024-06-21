06e49616811db5cb50b194399b0367575a16dcc2
Benjamin Laurent <blaurent@fu-r9-p4.s19.be>
2024-06-21 10:45:59 +0200

    fixed channels missing

--- services/backend/project/project/asgi.py
+++ services/backend/project/project/asgi.py
@@ -19,6 +19,7 @@
 import os
 import django
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
 
--- services/backend/project/project/routing.py
+++
@@ -1,7 +0,0 @@
-# routing.py
-from django.urls import re_path
-from .consumers import PingPongConsumer
-
-websocket_urlpatterns = [
-    re_path(r'ws/pingpong/$', PingPongConsumer.as_asgi()),
-]
