195a21417ebe6d938c907cd385ba651fd52b6e91
Benjamin Laurent <blaurent@fu-r9-p4.s19.be>
2024-06-21 13:20:37 +0200

    test

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -39,7 +39,7 @@ import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ThreejsGameAI from "./components/game/threejsai";
 import { useNavigate } from 'react-router-dom';
-
+// const backendHost = 'https://' + window.location.hostname + ':8000';
 const backendHost = 'https://' + window.location.hostname + ':8000';
 
 
